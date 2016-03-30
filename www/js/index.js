501ac66726bdea61e098e2d29e12ad3e03e382ef
Alex Berns <tagger94@gmail.com>
2016-03-30 20:39:39 +0000

    remove old code

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
 
         // Start the step counter
         // startingOffset will be added to the total steps counted in this session.
